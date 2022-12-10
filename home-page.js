b62c2909a4ed0ed6d94376c31fb4d4e4134e18d3
mariayaneva <maria04@abv.bg>
2022-12-10 18:22:49 +0200

    Added structure and basic design

---
+++ home-page.js
@@ -0,0 +1,10 @@
+const btn-play = document.getElementById("btn-play");
+btn-play.addEventListener("click", () => {
+    window.location.href = 'game-board.html';
+});
+
+// edit
+function playGame ()
+{
+    window.location.href('./game-board.html');
+}
