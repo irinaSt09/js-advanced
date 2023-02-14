5227e77d83e3dd27aa33bbfe149d8f0cdd61e4ec
Irina Stoeva <irina.stoeva@sap.com>
2023-02-14 20:47:16 +0200

    style game board

--- game/engine.js
+++ game/engine.js
@@ -5,4 +5,9 @@ const game = new Game();
 const board = document.getElementById("board");
 const cells = board.getElementsByTagName("td");
 
-game.start(cells)
+//game.start(cells)
+
+// Define startNewGame function in the global scope
+window.startNewGame = function() {
+    game.start(cells);
+};
